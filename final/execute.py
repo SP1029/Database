30fde30a11e0f76f709cd9341dc4a641307a4865
Harsh-Murdeshwar <harsharun21@iitk.ac.in>
2024-11-03 19:17:36 +0530

    corrected the code for year heuristics

--- final/execute.py
+++ final/execute.py
@@ -48,118 +48,109 @@ def get_years_in_range(node,ystart,yend):
             ans.extend(range(start, end + 1))
     return ans
 
+def name_only(op , value, idx):
+    ans = []
+    value = value.strip("'\"").lower()
+    # Exact match
+    if op == '=':
+        node = idx.global_trie.traverse(value)
+        if node and node.cnt > 0:
+            start = node.disklocstart
+            end = node.disklocstart + node.cnt - 1
+            ans = list(range(start, end + 1))
+        
+    elif op == 'LIKE':
+        # Prefix match
+        if value.endswith('%'):
+            prefix = value[:-1]
+            node = idx.global_trie.traverse(prefix)
+            if node and node.disklocstart != -1 and node.disklocend != -1:
+                ans = list(range(node.disklocstart, node.disklocend + 1))
+            
+           
+        # Exact match
+        else:
+            node = idx.global_trie.traverse(value)
+            if node and node.cnt > 0:
+                start = node.disklocstart
+                end = node.disklocstart + node.cnt - 1
+                ans = list(range(start, end + 1))
+    return ans
 
 def my_execute(clause, idx):
     
     ans = []
     
-    is_name = False
-    is_year = False
-
-    for predicate in clause:
-        field, op, value = predicate
-        if field == 'name':
-            is_name = True
-            n_op = op
-            n_value = value
-        elif field == 'year':
-            is_year = True
-            y_op = op
-            y_value = int(value)
-            if y_op == "=":
-                if not (idx.min_year<=y_value and y_value<=idx.max_year):
-                    return []
-            elif y_op == "<=":
-                if y_value < idx.min_year:
-                    return []
-                if y_value >= idx.max_year:
-                    is_year = False
-            elif y_op == ">=":
-                if y_value > idx.max_year:
-                    return []
-                if y_value <= idx.min_year:
-                    is_year = False
-    
-    # Name and Year Query
-    if is_name and is_year:
+    # Single
+    if len(clause)==1:
+        col, op, value = clause[0]
         
+        # Name only
+        if col=="name":
+            return name_only(op, value, idx)
+                    
+        # Year Only
+        else:
+            value = int(value)
+            match op:
+                case "=":
+                    ans = idx.num_buckets.get_eq(value)
+                    
+                case "<=":
+                    ans = idx.num_buckets.get_le(value)
+                    
+                case ">=":
+                    ans = idx.num_buckets.get_ge(value)
+
+    # Name and Year
+    else:
+        (_, n_op, n_value), (_, y_op, y_value) = clause
+        
+        y_value = int(y_value)
         n_value = n_value.strip("'\"").lower()
         
         # Year Filters
         if y_op == "=":
+            if not (idx.min_year<=y_value and y_value<=idx.max_year):
+                return []
             ystart = y_value
             yend = y_value
         elif y_op == "<=":
+            if y_value < idx.min_year:
+                return []
+            if y_value >= idx.max_year:
+                return name_only(n_op, n_value, idx)
             ystart = 1900
             yend = min(2100, y_value) 
         elif y_op == ">=":
+            if y_value > idx.max_year:
+                return []
+            if y_value <= idx.min_year:
+                return name_only(n_op, n_value, idx)
             ystart = max(1900, y_value)
             yend = 2100
+                
             
         if n_op == '=' or (n_op == 'LIKE' and not n_value.endswith('%')):
             node = idx.global_trie.traverse(n_value)
-            if node:
-                for buck in node.buckets:
-                    if buck.year < ystart: continue
-                    if buck.year > yend: break
-                    if buck.cnt > 0:
-                        start = buck.disklocstart
-                        end = start + buck.cnt - 1
-                        ans.extend(range(start, end + 1))
+            for buck in node.buckets:
+                if buck.year < ystart: continue
+                if buck.year > yend: break
+                if buck.cnt > 0:
+                    start = buck.disklocstart
+                    end = start + buck.cnt - 1
+                    ans.extend(range(start, end + 1))
                     
         elif n_op == 'LIKE':
             prefix = n_value[:-1]
-            node = idx.global_trie.traverse(prefix) 
-            if node:          
-                for buck in node.buckets:
-                    if buck.year < ystart: continue
-                    if buck.year > yend: break
-                    ans.extend(range(buck.disklocstart, buck.disklocend + 1))
+            node = idx.global_trie.traverse(prefix)            
+            for buck in node.buckets:
+                if buck.year < ystart: continue
+                if buck.year > yend: break
+                ans.extend(range(buck.disklocstart, buck.disklocend + 1))
                 
         ans = idx.checker.process(ans)
-    
-    # Name Only Query
-    elif is_name:
-        n_value = n_value.strip("'\"").lower()
-        # Exact match
-        if n_op == '=':
-            node = idx.global_trie.traverse(n_value)
-            if node and node.cnt > 0:
-                start = node.disklocstart
-                end = node.disklocstart + node.cnt - 1
-                ans = list(range(start, end + 1))
-            
-        elif n_op == 'LIKE':
-            # Prefix match
-            if n_value.endswith('%'):
-                prefix = n_value[:-1]
-                node = idx.global_trie.traverse(prefix)
-                if node and node.disklocstart != -1 and node.disklocend != -1:
-                    ans = list(range(node.disklocstart, node.disklocend + 1))
-                
-               
-            # Exact match
-            else:
-                node = idx.global_trie.traverse(n_value)
-                if node and node.cnt > 0:
-                    start = node.disklocstart
-                    end = node.disklocstart + node.cnt - 1
-                    ans = list(range(start, end + 1))
-    
-    # Year Ony Query
-    else:        
-        match y_op:
-            case "=":
-                ans = idx.num_buckets.get_eq(y_value)
-                
-            case "<=":
-                ans = idx.num_buckets.get_le(y_value)
-                
-            case ">=":
-                ans = idx.num_buckets.get_ge(y_value)
-
-    
-    
         
     return ans
+        
         
